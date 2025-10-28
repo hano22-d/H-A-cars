ab97cd7c443c3a5f5cf6164092b1d193efda72c4
hani joma <jomahani41@gmail.com>
2025-10-28 19:57:53 +0300

    تغييرات

--- news.js
+++ news.js
@@ -21,13 +21,42 @@ window.addEventListener('scroll',() => scroolbtn())
 
 /*برنامج تطبيق الثيم الليلي*/
 const darkmoodBtn = document.getElementById('darkmoodBtn');
-let iconChange = true;
+
+const themeSaved = localStorage.getItem('theme');
+let html = document.documentElement;
+
+
+if (themeSaved === 'dark') {
+  html.classList.add('darkmood')
+  darkmoodBtn.innerHTML = '<i class="fa-solid fa-sun"></i>'
+}
+else if (themeSaved === 'light') {
+  html.classList.add('lightmood')
+  darkmoodBtn.innerHTML = '<i class="fa-solid fa-moon"></i>'
+
+}
+
+let iconChange = themeSaved === 'dark' ? false : true;
+
 darkmoodBtn.onclick = _ => {
-  document.documentElement.classList.toggle('darkmood');
+  html.classList.toggle('lightmood');
+  html.classList.toggle('darkmood');
+
+  html.classList.contains('darkmood') ? localStorage.setItem('theme', 'dark')
+    : localStorage.setItem('theme', 'light')
+
   iconChange ? darkmoodBtn.innerHTML = '<i class="fa-solid fa-sun"></i>'
     : darkmoodBtn.innerHTML = '<i class="fa-solid fa-moon"></i>';
   iconChange = !iconChange
 }
+
+/*حفظ القيم في HeaderSearch*/
+const headerSubmit = document.getElementById('headerSubmit');
+const headerSearch = document.getElementById('headerSearch');
+const headerSearchSaved = sessionStorage.getItem('headerSearchValue');
+headerSearch.oninput = _ => sessionStorage.setItem('headerSearchValue', headerSearch.value);
+headerSearchSaved ? headerSearch.value = headerSearchSaved : '';
+
 
 
 
