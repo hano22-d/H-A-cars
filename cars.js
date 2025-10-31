bbaff2024622053124d18dcf12c2113551f33d7f
hani joma <jomahani41@gmail.com>
2025-10-31 20:08:02 +0200

    اضافة الترجمة للموقع

--- cars.js
+++ cars.js
@@ -89,3 +89,148 @@ function dinamicHeaderBackground() {
 dinamicHeaderBackground();
 
 setInterval(dinamicHeaderBackground, 15000);
+
+
+/*Language translation program*/
+
+//Translation file
+const information = {
+  en: {
+    home: 'home',
+    cars: 'cars',
+    compare: 'compare',
+    news: 'news',
+    store: 'store',
+    card1T: 'Mercedes-CLS-Class-2023',
+    card1P: 'germany',
+    list1: 'Engine capacity: 3.0L',
+    list2: 'Number of cylinders: 6',
+    list3: 'Engine power: 362 HP',
+    list4: 'Drive system: four-wheel drive',
+    faceBtn: 'More from Mercedes',
+    card2T: 'BMW-3-series-2024',
+    card2P: 'germany',
+    list5: 'Engine capacity: 3.0L',
+    list6: 'Number of cylinders: 4',
+    list7: 'Engine power: 255 HP',
+    list8: 'Drive system: rear',
+    faceBtn2: 'More from BMW',
+    card3T: 'Toyota-Camry-2025',
+    card3P: 'Japan',
+    list9: 'Engine capacity: 2.5L',
+    list10: 'Number of cylinders: 4',
+    list11: 'Engine power: 232 HP',
+    list12: 'Drive system: four-wheel drive',
+    faceBtn3: 'More from Toyota',
+    card4T: 'Ford-Edge-2024',
+    card4P: 'USA',
+    list13: 'Engine capacity: 2.0L',
+    list14: 'Number of cylinders: 4',
+    list15: 'Engine power: 250 HP',
+    list16: 'Drive system: four-wheel drive',
+    faceBtn4: 'More from Ford',
+    listdivTitle: 'Auto Archive',
+    make: 'Make',
+    model: 'Model',
+    year: 'Year',
+    go: 'Go',
+  },
+  ar: {
+    home: 'الرئيسية',
+    cars: 'السيارات',
+    compare: 'المقارنة',
+    news: 'الأخبار',
+    store: 'المتجر',
+
+    card1T: 'مرسيدس سي أل اس 2023',
+    card1P: 'ألمانيا',
+    list1: 'سعة المحرك: 3.0 لتر',
+    list2: 'عدد الأسطوانات: 6',
+    list3: 'قوة المحرك: 362 حصان',
+    list4: 'نظام الدفع: دفع رباعي',
+    faceBtn: 'المزيد من مرسيدس',
+
+    card2T: 'بي إم دبليو الفئة الثالثة 2024',
+    card2P: 'ألمانيا',
+    list5: 'سعة المحرك: 3.0 لتر',
+    list6: 'عدد الأسطوانات: 4',
+    list7: 'قوة المحرك: 255 حصان',
+    list8: 'نظام الدفع: خلفي',
+    faceBtn2: 'المزيد من بي إم دبليو',
+
+    card3T: 'تويوتا كامري 2025',
+    card3P: 'اليابان',
+    list9: 'سعة المحرك: 2.5 لتر',
+    list10: 'عدد الأسطوانات: 4',
+    list11: 'قوة المحرك: 232 حصان',
+    list12: 'نظام الدفع: دفع رباعي',
+    faceBtn3: 'المزيد من تويوتا',
+
+    card4T: 'فورد إيدج 2024',
+    card4P: 'الولايات المتحدة',
+    list13: 'سعة المحرك: 2.0 لتر',
+    list14: 'عدد الأسطوانات: 4',
+    list15: 'قوة المحرك: 250 حصان',
+    list16: 'نظام الدفع: دفع رباعي',
+    faceBtn4: 'المزيد من فورد',
+
+    listdivTitle: 'أرشيف السيارات',
+    make: 'الشركة',
+    model: 'الطراز',
+    year: 'السنة',
+    go: 'بحث',
+    p2: 'مرحبًا بك في H&A Cars',
+    p3: `  نحن هاني جمعة و علي سعدو، شغفنا بالسيارات كان الدافع الأكبر لإنشاء هذا
+        الموقع. هنا، نأخذك في جولة بين أحدث الطرازات، أخبار الصناعة، وتقنيات
+        المستقبل في عالم السيارات. هدفنا أن نصبح مرجعك الأول لكل ما تحتاجه عن
+        السيارات، من المواصفات والأسعار، إلى أحدث الإصدارات والنصائح الذكية.
+        في H&A Cars، نؤمن أن السيارة ليست مجرد وسيلة نقل، بل هي تجربة، قصة،
+        وعالم من الإبداع.`,
+
+    Services: 'الخدمات',
+    ServicesBlog1: 'خدمة البحث عن السيارات',
+    ServicesBlog2: 'تقييم السيارات المستعملة',
+    blog: 'المدونة',
+    ServicesBlog3: 'أخبار السيارات',
+    ServicesBlog4: 'دليل المشتري',
+    ServicesBlog5: 'مقارنة السيارات',
+
+    site: 'سياسة الموقع',
+    privacy1: 'سياسة الخصوصية',
+    privacy2: 'الشروط والأحكام',
+    privacy3: 'الكوكيز',
+  }
+}
+
+const select = document.getElementById('selectlan');
+let selector = document.querySelectorAll('[data-key]');
+
+let lansaved = localStorage.getItem('lan');
+
+function language(lang) {
+  if (lang === 'Arabic') {
+    selector.forEach(e => { e.textContent = information['ar'][e.dataset.key] });
+    document.querySelectorAll('p').forEach(a => { a.style.textAlign = 'right' });
+    document.querySelectorAll('h1').forEach(b => { b.style.textAlign = 'right' });
+    select.value = 'Arabic';
+  } else if (lang === 'English') {
+    selector.forEach(e => { e.textContent = information['en'][e.dataset.key] });
+    document.querySelectorAll('p').forEach(a => { a.style.textAlign = 'left' });
+    document.querySelectorAll('h1').forEach(b => { b.style.textAlign = 'left' });
+    select.value = 'English';
+  }
+}
+
+if (lansaved) {
+  language(lansaved);
+} else {
+
+  language('English');
+}
+
+select.addEventListener('change', () => {
+  let newLang = select.value;
+  language(newLang);
+  localStorage.setItem('lan', newLang);
+});
+
--- home.js
+++ home.js
@@ -166,3 +166,154 @@ setInterval(
   dinamicHeaderBackground, 15000
 )
 
+
+/*Language translation program*/
+
+//Translation file
+const information = {
+  en: {
+    home: 'home',
+    cars: 'cars',
+    compare: 'compare',
+    news: 'news',
+    store: 'store',
+    h2c: 'The Nio Firefly electric minicar – the first model from the Firefly brand (owned by Nio).',
+    paragrafa: `Basic information revealed by Nio on December 21, 2024,
+      with official deliveries starting on April 19, 2025, in China.
+      The starting price is around $16,770, and with the Battery-as-a-Service subscription option,
+      the lower price is about $11,100 per month. It will be launched in the European market during summer 2025 at v3cars.com.
+      ⚙️ Key specifications: a compact and modern hatchback design with the distinct Firefly logo,
+       not camouflaged with the traditional Nio badge. It focuses on energy efficiency,
+       with a fast and safe battery swap system through Nio's exchange network.`,
+    h2c2: 'Electric car manufacturing in Morocco',
+    paragrafb: `Basic information revealed by Nio on December 21, 2024, with official deliveries
+     starting on April 19, 2025, in China. The starting price is around $16,770, and with the Battery-as-a-Service
+      option, the lowest price is about $11,100 per month. It will be launched in the European market during
+       the summer of 2025. ⚙️ Key specifications: A modern compact hatchback design with the distinctive
+        Firefly logo, not camouflaged with the traditional Nio badge. It focuses on energy efficiency,
+         with a fast and safe battery swap system through Nio's exchange network.`,
+    titlemain3: 'The latest developments of next year...!',
+    bigPhotoText1: 'A fierce new competitor in the markets!',
+    bigPhotoText2: 'Palisade 2026, the latest extension of Hyundai`s luxury family SUV lineup',
+    pictureText1: 'Mercedes 2026 coming',
+    pictureParagraph1: `Mercedes-Benz will launch the 2026 S-Class with 6- and 8-cylinder engines delivering 496 and 442 hp.`,
+    pictureText2: 'The royal car has returned again!',
+    pictureParagraph2: 'Rolls-Royce announced a new release of its luxurious electric car, the Spectre.',
+    pictureText3: 'BMW refuses to back down',
+    pictureParagraph3: 'BMW is preparing to make a radical change in the world of electric cars with its new vehicle.',
+    titlemain5: 'Top 10 Popular Cars',
+    h2e: 'Photo gallery',
+    p2: 'Welcome to',
+    p3: `We are Hani Jomaa and Ali Saadou, and our passion for cars was the greatest motivation for
+     creating this website. Here, we take you on a tour of the latest models, industry news, and future
+      technologies in the automotive world. Our goal is to become your primary reference for everything
+       you need about cars, from specifications and prices to the latest releases and smart tips. At H&A Cars,
+        we believe that a car is not just a means of transportation, but an experience, a story, and a world
+         of creativity.`,
+    Services: 'Services',
+    ServicesBlog1: 'Car search service',
+    ServicesBlog2: 'Used car evaluation',
+    blog: 'blog',
+    ServicesBlog3: 'Automotive News',
+    ServicesBlog4: 'Buyer`s Guide',
+    ServicesBlog5: 'Car Comparison',
+    site: 'Site policies',
+    privacy1: 'Privacy Policy',
+    privacy2: 'Terms and Conditions',
+    privacy3: 'Cookies',
+  },
+  ar: {
+    home: 'الرئيسية',
+    cars: 'السيارات',
+    compare: 'المقارنة',
+    news: 'الأخبار',
+    store: 'المتجر',
+    h2c: 'سيارة Nio Firefly الكهربائية الصغيرة أول طراز من علامة Firefly التابعة لشركة Nio.',
+    paragrafa: `كشفت Nio عن المعلومات الأساسية في 21 ديسمبر 2024،
+  مع بدء التسليم الرسمي في 19 أبريل 2025 في الصين.
+  يبدأ سعر السيارة من حوالي 16,770 دولار،
+  ومع خيار الاشتراك في البطارية (Battery-as-a-Service)
+  يصبح سعرها حوالي 11,100 دولار شهريًا. سيتم إطلاقها في السوق الأوروبية خلال صيف 2025 عبر v3cars.com.
+  ⚙️ المواصفات الأساسية: تصميم هاتشباك صغير وعصري مع شعار Firefly الجديد،
+  وليس شعار Nio التقليدي. تركّز السيارة على الكفاءة في استهلاك الطاقة،
+  وتدعم نظام تبديل البطاريات السريع عبر شبكة Nio لتغيير البطاريات.`,
+
+    h2c2: 'تصنيع السيارات الكهربائية في المغرب',
+    paragrafb: ` أعلنت مؤخّرًا عن استثمار بقيمة 1.2 مليار يورو لتوسيع مصنعها في
+            القنيطرة، المغرب، بهدف زيادة الإنتاج السنوي من 20,000 إلى 70,000
+            سيارة كهربائية من نماذج مثلStellantis Citroën Ami، Opel Rocks‑e،
+            وFiat Topolino Reuters . تأتي هذه الخطوة كجزء من استراتيجية المغرب
+            لتعزيز صناعة السيارات المحلية؛ مع هدف رفع نسبة المكونات المصنعة
+            محليًا إلى 75% بحلول 2030، مقارنةً بحاليًا حوالي 69% Reuters .
+            توسعة المصنع الخامسة منذ افتتاحه عام 2019، وهي خطوة تؤكد مكانة
+            المغرب كمركز إنتاج عالمي للسيارات الكهربائية.`,
+    titlemain3: 'أحدث تطورات العام القادم',
+    bigPhotoText1: 'منافس شرس جديد في الأسواق!',
+    bigPhotoText2: 'باليسيد 2026 الامتداد الأحدث لسلسلة سيارات الدفع الرباعي العائلية الفاخرة من هيونداي',
+
+    pictureText1: 'مرسيدس 2026 قادمة قريبًا',
+    pictureParagraph1: `تستعد مرسيدس بنز لإطلاق سيارتها الجديدة اس كلاس 2026 بمحرك 6
+                و8 سلندر بقوة 496 و442 حصان`,
+
+    pictureText2: 'عودة السيارة الملكية من جديد',
+    pictureParagraph2: 'أعلنت شركة رولز رويس عن إصدار جديد من سياراتها الكهربائية الفاخرة، سبيكتر.',
+
+    pictureText3: 'BMW ترفض التراجع',
+    pictureParagraph3: 'تستعد شركة بي أم دبليو لإحداث تغيير جذري في عالم السيارات الكهربائية من خلال سيارتها الجديدة',
+
+    titlemain5: 'أفضل 10 سيارات شهيرة',
+    h2e: 'معرض الصور',
+
+    p2: 'مرحبًا بك في H&A Cars',
+    p3: `  نحن هاني جمعة و علي سعدو، شغفنا بالسيارات كان الدافع الأكبر لإنشاء هذا
+        الموقع. هنا، نأخذك في جولة بين أحدث الطرازات، أخبار الصناعة، وتقنيات
+        المستقبل في عالم السيارات. هدفنا أن نصبح مرجعك الأول لكل ما تحتاجه عن
+        السيارات، من المواصفات والأسعار، إلى أحدث الإصدارات والنصائح الذكية.
+        في H&A Cars، نؤمن أن السيارة ليست مجرد وسيلة نقل، بل هي تجربة، قصة،
+        وعالم من الإبداع.`,
+
+    Services: 'الخدمات',
+    ServicesBlog1: 'خدمة البحث عن السيارات',
+    ServicesBlog2: 'تقييم السيارات المستعملة',
+    blog: 'المدونة',
+    ServicesBlog3: 'أخبار السيارات',
+    ServicesBlog4: 'دليل المشتري',
+    ServicesBlog5: 'مقارنة السيارات',
+
+    site: 'سياسة الموقع',
+    privacy1: 'سياسة الخصوصية',
+    privacy2: 'الشروط والأحكام',
+    privacy3: 'الكوكيز',
+  }
+}
+const select = document.getElementById('selectlan');
+let selector = document.querySelectorAll('[data-key]');
+
+let lansaved = localStorage.getItem('lan');
+
+function language(lang) {
+  if (lang === 'Arabic') {
+    selector.forEach(e => { e.textContent = information['ar'][e.dataset.key] });
+    document.querySelectorAll('p').forEach(a => { a.style.textAlign = 'right' });
+    document.querySelectorAll('h1').forEach(b => { b.style.textAlign = 'right' });
+    select.value = 'Arabic';
+  } else if (lang === 'English') {
+    selector.forEach(e => { e.textContent = information['en'][e.dataset.key] });
+    document.querySelectorAll('p').forEach(a => { a.style.textAlign = 'left' });
+    document.querySelectorAll('h1').forEach(b => { b.style.textAlign = 'left' });
+    select.value = 'English';
+  }
+}
+
+if (lansaved) {
+  language(lansaved);
+} else {
+
+  language('English');
+}
+
+select.addEventListener('change', () => {
+  let newLang = select.value;
+  language(newLang);
+  localStorage.setItem('lan', newLang);
+});
